030f6dc097a3fc8889323b45356fa2f789b64186
Jeff <jmartinbaldwin@gmail.com>
2026-04-07 15:16:25 +0900

    Implement my_each and my_map

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,36 @@
 module Enumerable
-  # Your code goes here
+
+  def my_map
+    return to_enum(:my_map) unless block_given?
+
+    new_array = []
+
+    my_each do |element|
+      new_array << yield(element)
+    end
+    new_array
+  end 
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
 end
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
+
 class Array
-  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+
+    self
+  end
 end
